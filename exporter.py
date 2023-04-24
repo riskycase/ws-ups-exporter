5abe118e41c1c8dc8dbc8a4396411a7febda1ace
Artem Pastukhov <artem.pastukhov@gmail.com>
2023-04-24 09:36:08 +0300

    Cleanup

--- exporter.py
+++ exporter.py
@@ -13,17 +13,11 @@ busVoltage.set_function(lambda: ina.getBusVoltage_V())
 shuntVoltage = Gauge('shunt_voltage_mv', 'Shunt Voltage')
 shuntVoltage.set_function(lambda: ina.getShuntVoltage_mV())
 
-# psuVoltage = Gauge('psu_voltage_mv', 'PSU Voltage')
-# psuVoltage.set_function(lambda: getPSUVoltage())
-
 power = Gauge('power_w', 'Power')
 power = power.set_function(lambda: ina.getPower_W())
 
 current = Gauge('current_ma', 'Current')
 current = current.set_function(lambda: ina.getCurrent_mA())
-
-# def getPSUVoltage():
-#     return ina.getBusVoltage_V() + ( ina.getShuntVoltage_mV() / 1000.0 )
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
